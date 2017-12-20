b1f4921d96793ad65a09e843949a7bc255c545ab
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2017-12-20 12:13:20 +0100

    Add initial project files

---
+++ src/PatchKit.IssueReporting/Issue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PatchKit.IssueReporting
+{
+    public class Issue
+    {
+        public string Message { get; set; }
+
+        public Exception Exception { get; set; }
+
+        public IDictionary<string, string> Tags { get; set; }
+    }
+}
---
+++ src/PatchKit.IssueReporting/IssueReporterExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace PatchKit.IssueReporting
+{
+    public static class IssueReporterExtensions
+    {
+        public static void Report([NotNull] IIssueReporter @this, string message, Exception exception)
+        {
+            @this.Report(new Issue
+            {
+                Message = message,
+                Exception = exception
+            });
+        }
+
+        public static void Report([NotNull] IIssueReporter @this, Exception exception)
+        {
+            @this.Report(new Issue
+            {
+                Exception = exception
+            });
+        }
+
+        public static void Report([NotNull] IIssueReporter @this, Exception exception, IDictionary<string, string> tags)
+        {
+            @this.Report(new Issue
+            {
+                Exception = exception,
+                Tags = tags
+            });
+        }
+
+        public static void Report([NotNull] IIssueReporter @this, string message, Exception exception, IDictionary<string, string> tags)
+        {
+            @this.Report(new Issue
+            {
+                Message = message,
+                Exception = exception,
+                Tags = tags
+            });
+        }
+    }
+}
